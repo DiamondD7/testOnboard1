7b4f295e33e53cdb94b14d42213cf39a1409f425
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2022-06-30 18:42:46 +1200

    added sales

---
+++ testOnboard1/Models/Sales.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testOnboard1.Models
+{
+    public class Sales
+    {
+        [Key]
+        public int SalesId { get; set; }
+        public string Customer { get; set; }
+        public string Store { get; set; }
+        public string Product { get; set; }
+        public string DateSold { get; set; }
+
+
+    }
+}
