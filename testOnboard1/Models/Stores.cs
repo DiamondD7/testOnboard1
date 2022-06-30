c430f675270f218d78305bc7e55e2f0f386c06ab
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2022-06-30 18:41:29 +1200

    added stores

---
+++ testOnboard1/Models/Stores.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testOnboard1.Models
+{
+    public class Stores
+    {
+        [Key]
+        public int StoreId { get; set; }
+        public string StoreName { get; set; }
+        public string StoreAddress { get; set; }
+
+    }
+}
