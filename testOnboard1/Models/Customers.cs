6197afb7ac1536d2606dcd21e28221fdce64a2ac
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2022-06-30 18:25:30 +1200

    Add project files.

---
+++ testOnboard1/Models/Customers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testOnboard1.Models
+{
+    public class Customers
+    {
+    }
+}
