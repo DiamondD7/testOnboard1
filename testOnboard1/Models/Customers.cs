a3bcddbb1b6204f5a9f5e77d35f86da158f887c4
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2022-06-30 18:36:31 +1200

    created model for customers

--- testOnboard1/Models/Customers.cs
+++ testOnboard1/Models/Customers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,9 @@ namespace testOnboard1.Models
 {
     public class Customers
     {
+        [Key]
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; }
     }
 }
