e4579c798fa0be845e9c80b575bdfa7d81676e60
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2022-06-30 18:37:28 +1200

    added product mdoels

---
+++ testOnboard1/Models/Products.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testOnboard1.Models
+{
+    public class Products
+    {
+        [Key]
+        public int ProductsId { get; set; }
+        public string ProductsName { get; set; }
+        public int ProductPrice { get; set; }
+    }
+}
